bc820580f57b42c11267c6ed4dce1dafadf49f56
ronald.joyner-brown <r.brownjoyner@gmail.com>
2024-09-16 12:49:05 -0400

    Initial commit

---
+++ src/main/java/com/perscholas/Doctor.java
@@ -0,0 +1,9 @@
+package com.perscholas;
+
+public class Doctor {
+    String DoctorName;
+    String Department;
+    public void Doctor_Details(){
+        System.out.println("Doctor Details...");
+    }
+}
